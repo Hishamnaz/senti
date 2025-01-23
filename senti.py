03e58196cbb95d1a17e28c383bebed0a30aeb57d
Hishamnaz <165064414+Hishamnaz@users.noreply.github.com>
2025-01-23 21:57:17 +0530

    Update senti.py

--- senti.py
+++ senti.py
@@ -255,7 +255,7 @@ def translate_text(text):
 
 def get_request_headers():
     return {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
     }
 
 async def single_page_scrape(url, page_number, encountered_reviews):
